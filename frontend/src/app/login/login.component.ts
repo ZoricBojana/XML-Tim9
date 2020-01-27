20be9df7442cb2e5b6bf18613581c2b438921f5d
m-jordan <57231548+m-jordan@users.noreply.github.com>
2020-01-27 19:16:28 +0100

    projekat front

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+}
